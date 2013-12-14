495b50cb2de6cec08d85ec65304f5d4a44994e65
Gustavo Cortez <cmgustavo83@gmail.com>
2013-12-14 13:34:41 -0300

    google maps location

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,16 @@
  * GET home page.
  */
 
+var gm = require('googlemaps');
+var util = require('util');
+
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
+};
+
+exports.location = function(req, res){
+    gm.reverseGeocode(gm.checkAndConvertPoint([41.850033, -87.6500523]), function(err, data){
+        util.puts(JSON.stringify(data));
+        res.json('index', { location: data });
+    });
 };
